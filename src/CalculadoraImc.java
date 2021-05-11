8b5d65771263eeec22034f78f86d03ad0685a032
leandro.azevedo <leandro.azevedo@yahoo.com.br>
2021-05-11 15:21:32 -0300

    incluindo locale

--- src/CalculadoraImc.java
+++ src/CalculadoraImc.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CalculadoraImc {
 
     public static void main(String[] args){
 
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
         // ENTRADA DE DADOS
--- src/Formatacao.java
+++ src/Formatacao.java
@@ -1,12 +1,15 @@
+import java.util.Locale;
+
 public class Formatacao {
 
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
 
         double x = 12.345678;
 
         System.out.print(x); //imprime sem quebrar linha
         System.out.println(x); //imprime e insere quebra de linha
-        
+
         System.out.printf("%.2f%n" , x); //imprime com 2 casas decimais
         System.out.printf("%.4f%n" , x); //imprime com 4 casas decimais
     }
