781f7ec062c799cbf037a92c8e5187c51147ffb4
Your Name <you@example.com>
2021-06-09 21:17:45 -0300

    Exercicio 4

---
+++ src/estruturarepeticao/enquanto/Ex4.java
@@ -0,0 +1,13 @@
+package estruturarepeticao.enquanto;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5){
+            int y = x * 3;
+            System.out.print(y);
+            x = x + 1;
+        }
+        System.out.println("Fim");
+    }
+}
