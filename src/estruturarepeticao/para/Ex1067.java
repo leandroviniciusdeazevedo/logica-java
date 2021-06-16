961d6fc21c6fd115a73ee1153b7f0ff40862076b
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 20:48:09 -0300

    Exercicio 1067 if adicionado

--- src/estruturarepeticao/para/Ex1067.java
+++ src/estruturarepeticao/para/Ex1067.java
@@ -6,8 +6,8 @@ public class Ex1067 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        for (int i = 1; i <= x; i++){
-            if (i % 2 != 0){
+        if (x >= 1 && x <= 1000){
+            for (int i = 1; i <= x; i += 2){
                 System.out.println(i);
             }
         }
